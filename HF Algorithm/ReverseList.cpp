bc048a92a0cb9a2d3547460277053f8ca8dc28cc
Liuyf0 <2743357832@qq.com>
2023-09-11 10:09:53 +0800

    Modify ReverseList

--- HF Algorithm/ReverseList.cpp
+++ HF Algorithm/ReverseList.cpp
@@ -5,23 +5,15 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-//头插法来做，将元素开辟在栈上，这样会避免内存泄露
-ListNode* ReverseList(ListNode* pHead) {
-
-	// 头插法
-	if (pHead == nullptr || pHead->next == nullptr) return pHead;
-	ListNode dummyNode = ListNode(0);
-	ListNode* pre = &(dummyNode);
-	pre->next = pHead;
-	ListNode* cur = pHead->next;
-	pHead->next = nullptr;
-	//pre = cur;
-	ListNode* temp = nullptr;
-	while (cur != nullptr) {
-		temp = cur;
-		cur = cur->next;
-		temp->next = pre->next;
-		pre->next = temp;
-	}
-	return dummyNode.next;
+ListNode* reverseList(ListNode* head) {
+	ListNode* pre = nullptr;
+	if(!head) return head;
+	ListNode* index = head, *ord = index->next;
+	while(index) {
+		index->next = pre;
+		pre = index;
+		index = ord;
+		if(ord) ord = ord->next;
+	} 
+	return pre;
 }
