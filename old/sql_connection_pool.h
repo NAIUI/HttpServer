a936bfc0d7b3073b4150cbcf833ebb638ee3ad24
Liuyf0 <2743357832@qq.com>
2023-12-05 19:29:40 -0800

    add old version code

---
+++ old/sql_connection_pool.h
@@ -0,0 +1,62 @@
+#ifndef _CONNECTION_POOL_
+#define _CONNECTION_POOL_
+
+#include <stdio.h>
+#include <list>
+#include <mysql/mysql.h>
+#include <error.h>
+#include <string.h>
+#include <iostream>s
+#include <string>
+#include "locker.h"
+#include "log.h"
+
+using namespace std;
+
+class connection_pool
+{
+public:
+	MYSQL *GetConnection();				 //获取数据库连接
+	bool ReleaseConnection(MYSQL *conn); //释放连接
+	int GetFreeConn();					 //获取连接
+	void DestroyPool();					 //销毁所有连接
+
+	//单例模式
+	static connection_pool *GetInstance();
+
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); 
+
+private:
+	connection_pool();
+	~connection_pool();
+
+	int m_MaxConn;  //最大连接数
+	int m_CurConn;  //当前已使用的连接数
+	int m_FreeConn; //当前空闲的连接数
+	locker lock;
+	list<MYSQL *> connList; //连接池
+	sem reserve;			// 连接池同步锁
+
+public:
+	string m_url;			// 主机地址
+	string m_Port;		 	// 数据库端口号
+	string m_User;		 	// 登陆数据库用户名
+	string m_PassWord;	 	// 登陆数据库密码
+	string m_DatabaseName; 	// 使用数据库名
+	int m_close_log;		// 日志开关
+};
+
+// 将数据库连接的获取与释放通过RAII机制封装，避免手动释放。
+class connectionRAII{
+
+public:
+	//双指针对MYSQL *con修改
+	connectionRAII(MYSQL **con, connection_pool *connPool);
+	~connectionRAII();
+	
+private:
+	MYSQL *conRAII;
+	connection_pool *poolRAII;
+};
+
+#endif
